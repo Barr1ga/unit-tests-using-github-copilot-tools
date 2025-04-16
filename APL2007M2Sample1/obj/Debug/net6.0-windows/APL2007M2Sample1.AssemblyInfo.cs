246ea7ef87c9cf3c67b2d1022cfa61e044f62e5f
Horeb Barriga <hbarriga@fullscale.ph>
2025-04-16 13:52:55 +0800

    Add generated files for APL2007M3SalesReport-InlineChat project
    - Created GlobalUsings.g.cs for global using directives.
    - Added assets.cache for project assets.
    - Included CoreCompileInputs.cache for compile inputs.
    - Generated FileListAbsolute.txt listing all project files.
    - Added compiled DLL for APL2007M3SalesReport-InlineChat.

--- APL2007M2Sample1/obj/Debug/net6.0-windows/APL2007M2Sample1.AssemblyInfo.cs
+++ APL2007M2Sample1/obj/Debug/net6.0-windows/APL2007M2Sample1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APL2007M2Sample1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9df35f066f51c64f0c3e7c4026083c14bf12db4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("APL2007M2Sample1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APL2007M2Sample1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
